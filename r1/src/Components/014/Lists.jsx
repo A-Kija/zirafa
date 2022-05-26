b21fcc4f890b17159105c04130251c9b953545d0
audronemotiejunaite <89936835+audronemotiejunaite@users.noreply.github.com>
2022-05-26 22:15:28 +0300

    viskas

--- r1/src/Components/014/Lists.jsx
+++ r1/src/Components/014/Lists.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function Lists() {
 
     const sizes = [
@@ -9,8 +11,11 @@ function Lists() {
         'XXL'
     ];
 
+    const [show, setShow] = useState(false);
+
     return (
         <div class="lc">
+            <button className="a" onClick={()=>setShow(s => !s)}>Show</button>
             <div class="list">
             {
                 sizes.map((s, i)=><div key={i}>{s}</div>)
@@ -23,6 +28,30 @@ function Lists() {
                     <div key={i} style={{color:'red'}}>{s}</div>
                     )
                 )
+            }
+            </div>
+            <div class="list">
+            {
+                sizes.map((s, i)=> (
+                    i % 2 ? <div key={i} style={{color:'yellow'}}><i>{s}</i></div>:
+                    <div key={i} style={{color:'red'}}><u>{s}</u></div>
+                    )
+                )
+            }
+            </div>
+            <div class="list">
+            {
+                sizes.filter(s => s[0]==='X').map((s, i)=><div key={i}>{s}</div>)
+            }
+            </div>
+            <div class="list">
+            {
+                sizes.sort((a, b) => b.length - a.length).map((s, i)=><div key={i}>{s}</div>)
+            }
+            </div>
+            <div class="list">
+            {
+                sizes.filter(_ => show).map((s, i)=><div key={i}>{s}</div>)
             }
             </div>
         </div>
