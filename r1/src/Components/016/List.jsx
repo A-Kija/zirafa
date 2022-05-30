22b94e863db39b66069895d302980910a4f2203a
audronemotiejunaite <89936835+audronemotiejunaite@users.noreply.github.com>
2022-05-30 20:45:28 +0300

    del

--- r1/src/Components/016/List.jsx
+++ r1/src/Components/016/List.jsx
@@ -1,6 +1,8 @@
 function List({obj, index}) {
     return (
-        <li className="list-group-item"><b>#{index}</b> {obj.name} {obj.color} <small>id:{obj.id}</small></li>
+        <li className="list-group-item"><b>#{index}</b> {obj.name} {obj.color} <small>id:{obj.id}</small>
+        <button type="button" className="btn btn-outline-danger mt-4 mr-2" onClick={() => console.log(obj.id)}>Delete</button>
+        </li>
     )
 }
 
