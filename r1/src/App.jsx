9d6c8c5b9035a942ca94306a09c682f979b3f909
audronemotiejunaite <89936835+audronemotiejunaite@users.noreply.github.com>
2022-05-30 18:20:12 +0300

    list

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,40 +1,44 @@
-import { useState } from 'react';
+import './bootstrap.css';
 import './App.scss';
-import List2 from './Components/015/List2';
 function App() {
 
-    const cars = [
-        {id: 1, type: 'MB', color: 'blue', price: 45874.01},
-        {id: 4, type: 'Volvo', color: 'pink', price: 54658.00},
-        {id: 17, type: 'MB', color: 'black', price: 6465.77},
-        {id: 47, type: 'Opel', color: 'blue', price: 89791.77},
-        {id: 10, type: 'Scania', color: 'red', price: 44624.00},
-        {id: 107, type: 'BMW', color: 'red', price: 98591.00},
-        {id: 44, type: 'BMW', color: 'yellow', price: 789.30},
-        {id: 78, type: 'MB', color: 'blue', price: 64598.00},
-        {id: 99, type: 'Tesla', color: 'gray', price: 78944.00}
-    ];
-
-    // const [showCar, setShowCar] = useState(null);
-
-    // const [activeCar, setActiveCar] = useState(10);
-
-    const [kv, setKv] = useState([]);
-
     return (
-        <div className="App">
-            <header className="App-header">
-                <h1>REPEAT PART II</h1>
-                <div className="kvc">
-                {
-                    kv.map((k, i) => <div style={{background:k}} key={i} className="kv"></div>)
-                }
+        <>
+            <div className="container">
+                <div className="row">
+                    <div className="col-4">
+                        <div className="card m-4">
+                            <div className="card-header">
+                                Add to list
+                            </div>
+                            <div className="card-body">
+                                <div className="form-group">
+                                    <label>Name</label>
+                                    <input type="text" className="form-control" />
+                                </div>
+                                <div className="form-group">
+                                    <label>Color</label>
+                                    <input type="text" className="form-control" />
+                                </div>
+                                <button type="button" className="btn btn-outline-primary">ADD</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-8">
+                        <div className="card m-4">
+                            <div className="card-header">
+                                List
+                            </div>
+                            <div className="card-body">
+                                <ul className="list-group">
+                                    <li className="list-group-item">An item</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <button className="a" onClick={() => setKv(k => [...k, 'skyblue', 'gray'])}>ADD []</button>
-                {/* <h2>{showCar ? showCar.type : null}</h2> */}
-                {/* <List2 cars={cars} setShowCar={setShowCar} activeCar={activeCar} setActiveCar={setActiveCar}></List2> */}
-            </header>
-        </div>
+            </div>
+        </>
     );
 
 }
