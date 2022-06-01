d09baaeb81fbcb54b99133fc468fe6d402f0778d
audronemotiejunaite <89936835+audronemotiejunaite@users.noreply.github.com>
2022-06-01 18:57:54 +0300

    read

--- server/app.js
+++ server/app.js
@@ -1,12 +1,46 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3003;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-  
-})
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
+const cors = require("cors");
+app.use(cors());
+
+const mysql = require("mysql");
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "labas",
+});
+
+// READ
+app.get("/trees", (req, res) => {
+  const sql = `
+  SELECT
+  *
+  FROM trees
+`;
+  con.query(sql, (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+});
+
+
+
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 });
