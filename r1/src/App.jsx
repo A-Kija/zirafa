8e532b46b01163ba4a4b160852174b41f503d1ff
A-Kija <arvydas.kijakauskas@gmail.com>
2022-06-09 19:51:00 +0300

    contex

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,26 +1,33 @@
 import { useState } from 'react';
 import './App.scss';
 import Number3 from './Components/023/Number3';
+import Number5 from './Components/023/Number5';
 import Te from './Components/023/Te';
 
 function App() {
 
     const [number, setNumber] = useState(1);
-    const [number3, setNumber3] = useState(1);
+    const [number3, setNumber3] = useState(3);
+    const [number5, setNumber5] = useState(1);
+    const [number7, setNumber7] = useState(7);
 
     return (
-        <Number3.Provider value={number3}>
+        <Number5.Provider value={number5}>
+        <Number3.Provider value={{number3, number7}}>
         <div className="App">
             <header className="App-header">
                 <div className="kvc">
                     <button className="a b" onClick={() => setNumber(n => n + 1)}>+1</button>
                     <button className="a b" onClick={() => setNumber3(n => n + 3)}>+3</button>
+                    <button className="a b" onClick={() => setNumber5(n => n + 5)}>+5</button>
+                    <button className="a b" onClick={() => setNumber7(n => n + 7)}>+7</button>
                 </div>
                 <h1>Context</h1>
                 <Te number={number}></Te>
             </header>
         </div>
         </Number3.Provider>
+        </Number5.Provider>
     );
 }
 
--- r1/src/Components/023/Va.jsx
+++ r1/src/Components/023/Va.jsx
@@ -1,14 +1,23 @@
 import { useContext } from "react";
 import Number3 from "./Number3";
+import Number5 from "./Number5";
 
-function Va({number}) {
+function Va({ number }) {
 
-    const number3 = useContext(Number3);
+    const { number3, number7 } = useContext(Number3);
 
     return (
         <>
             <h2>{number}</h2>
-            <h2>{number3}</h2>
+            <Number5.Consumer>
+                {
+                    value => <h2>{value}</h2>
+                }
+            </Number5.Consumer>
+            <h2>3: {number3}</h2>
+            <h2>7: {number7}</h2>
+
+
         </>
     );
 }
