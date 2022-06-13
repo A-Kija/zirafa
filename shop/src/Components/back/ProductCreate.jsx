c80ba94eee9ec680e1234c4ea51a78fafd275d43
A-Kija <arvydas.kijakauskas@gmail.com>
2022-06-13 19:29:34 +0300

    create

--- shop/src/Components/back/ProductCreate.jsx
+++ shop/src/Components/back/ProductCreate.jsx
@@ -1,4 +1,28 @@
+import { useContext, useState } from "react";
+import BackContext from "../../Contexts/BackContext";
+
+const empty = {
+    title: '',
+    price: '',
+    code: '',
+    description: ''
+};
+
+
+
 function ProductCreate() {
+
+    const {setCreateProductData} = useContext(BackContext);
+
+    const [inputs, setInputs] = useState(empty);
+
+    const handleInputs = (e, input) => setInputs(i => ({...i, [input]: e.target.value}));
+
+    const create = () => {
+        setCreateProductData(inputs);
+        setInputs(empty);
+    }
+
 
     return (
         <div className="col-5">
@@ -12,30 +36,30 @@ function ProductCreate() {
                             <div className="col-12">
                                 <div className="form-group">
                                     <label className="fu gray">Pavadinimas:</label>
-                                    <input type="text" className="form-control" />
+                                    <input type="text" className="form-control" value={inputs.title} onChange={e => handleInputs(e, 'title')} />
                                 </div>
                             </div>
                             <div className="col-4">
                                 <div className="form-group">
                                     <label className="fu gray">Kaina:</label>
-                                    <input type="text" className="form-control" />
+                                    <input type="text" className="form-control" value={inputs.price} onChange={e => handleInputs(e, 'price')} />
                                 </div>
                             </div>
                             <div className="col-8">
                                 <div className="form-group">
                                     <label className="fu gray">Kodas:</label>
-                                    <input type="text" className="form-control" />
+                                    <input type="text" className="form-control" value={inputs.code} onChange={e => handleInputs(e, 'code')} />
                                 </div>
                             </div>
                             <div className="col-12">
                                 <div className="form-group">
                                     <label className="fu gray">Aprašymas:</label>
-                                    <textarea className="form-control" rows="3"></textarea>
+                                    <textarea className="form-control" rows="3" value={inputs.description} onChange={e => handleInputs(e, 'description')}></textarea>
                                 </div>
                             </div>
                             <div className="col-12">
                                 <div className="mt-3">
-                                <button type="button" className="btn btn-outline-dark fu up">Sukurti</button>
+                                <button type="button" className="btn btn-outline-dark fu up" onClick={create}>Sukurti</button>
                                 </div>
                             </div>
                         </div>
