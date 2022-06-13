fbb97566fb308bf81cce522dc238983521faf1b5
A-Kija <arvydas.kijakauskas@gmail.com>
2022-06-13 20:30:10 +0300

    message

--- shop/src/Components/back/Back.jsx
+++ shop/src/Components/back/Back.jsx
@@ -9,18 +9,19 @@ import ProductCreate from './ProductCreate';
 function Back() {
 
     const [products, setProducts] = useState(null);
+    const [lastProductsUpdate, setLastProductsUpdate] = useState(Date.now());
 
     const [createProductData, setCreateProductData] = useState(null);
 
     useEffect(() => {
         axios.get('http://localhost:3003/admin/products')
             .then(res => setProducts(res.data));
-    }, []);
+    }, [lastProductsUpdate]);
 
     useEffect(() => {
         if (createProductData === null) return;
         axios.post('http://localhost:3003/admin/products', createProductData)
-            .then(res => console.log(res.data));
+            .then(_ => setLastProductsUpdate(Date.now()));
     }, [createProductData]);
 
     return (
