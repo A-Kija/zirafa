7295c0d7b994e319e740cd4dc1fc96c9419d75e5
A-Kija <arvydas.kijakauskas@gmail.com>
2022-06-15 20:05:54 +0300

    css image

--- shop/src/Components/front/Product.jsx
+++ shop/src/Components/front/Product.jsx
@@ -1,9 +1,26 @@
-function Product({product}) {
+function Product({ product }) {
 
     return (
         <div className="product">
             <div className="product__bin">
-            {product.title}
+                <div className="product__bin__top">
+                    <h2>{product.title}</h2><i>{product.code}</i>
+                </div>
+                <div className="product__bin__mid">
+                    <div className="product__bin__mid__img">
+                        {
+                            product.photo ? <img src={product.photo} alt={product.title} /> : <img src="http://localhost:3000/no-image.jpg" alt={product.title} />
+                        }
+                        
+                    </div>
+                    <div className="product__bin__mid__desc">
+                        <p>{product.description}</p>
+                    </div>
+                </div>
+                <div className="product__bin__bottom">
+                    <span>{product.price}</span>
+                    <button>Pirkti</button>
+                </div>
             </div>
         </div>
     );
