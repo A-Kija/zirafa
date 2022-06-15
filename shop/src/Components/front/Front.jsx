63b83861a7e2fad3d86cf62beadae85417f49b32
A-Kija <arvydas.kijakauskas@gmail.com>
2022-06-15 18:59:11 +0300

    push

--- shop/src/Components/front/Front.jsx
+++ shop/src/Components/front/Front.jsx
@@ -1,15 +1,28 @@
+import { useReducer } from 'react';
+import FrontContext from '../../Contexts/FrontContext';
 import '../../front.scss';
+import productsReducer from '../../Reducers/productsReducer';
+import Loader from './Loader';
 import Products from './Products';
 import TopBar from './TopBar';
 
+
+
 function Front() {
+
+  const [products, dp] = useReducer(productsReducer, null);
   return (
+    <FrontContext.Provider value={{}}>
     <div id="shop">
       <div className="bin">
         <TopBar></TopBar>
-        <Products></Products>
+        {
+          products !== null ? <Products></Products> : <Loader></Loader>
+        }
+        
       </div>
     </div>
+    </FrontContext.Provider>
   );
 }
 
---
+++ shop/src/Reducers/productsReducer.js
@@ -0,0 +1,10 @@
+function productsReducer(state, action) {
+    let newState;
+    switch (action.type) {
+
+
+        default: newState = [...state];
+    }
+    return newState;
+}
+export default productsReducer;
